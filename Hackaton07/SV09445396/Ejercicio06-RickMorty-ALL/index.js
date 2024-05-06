bd3fe8adc09421ff4a4101cae6fd33ccdceb5edd
Jose Balcazar <jossecap@hotmail.com>
2024-05-05 22:53:31 -0500

    Ejercicios resueltos de la Hackaton07 en JavaScript y NodeJs

---
+++ Hackaton07/SV09445396/Ejercicio06-RickMorty-ALL/index.js
@@ -0,0 +1,44 @@
+console.log("Inicio de la aplicacion");
+var http = require('http');
+const axios = require('axios');
+var url = require('url');
+
+http.createServer(async function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    var param = url.parse(req.url, true).query;
+
+    console.log(param);
+
+    let strUrl = req.url;
+
+    if (strUrl.includes("personajes")) {
+        const options = {
+            method: 'GET',
+            url: 'https://rickandmortyapi.com/api/character/1,2,3,4,5',
+            params: {
+            },
+            headers: {
+            }
+        };
+
+        axios.request(options)
+            .then((response) => {
+                let objRespuesta = {
+                    personajes: response.data
+                }
+               res.write(JSON.stringify(objRespuesta));
+                res.end();
+            })
+            .catch((error) => {
+                console.log("Hubo un error en la aplicacion")
+                console.log(error);
+            });
+    }
+    else {
+        res.write(JSON.stringify({ data: "Faltan datos!" }));
+        res.end();
+    }
+
+}).listen(8080);
+
---
+++ Hackaton07/SV09445396/Ejercicio10-Fotos/index.js
@@ -0,0 +1,44 @@
+console.log("Inicio de la aplicacion");
+var http = require('http');
+const axios = require('axios');
+var url = require('url');
+
+http.createServer(async function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    var param = url.parse(req.url, true).query;
+
+    console.log(param);
+
+    let strUrl = req.url;
+
+    if (strUrl.includes("foto")) {
+        const options = {
+            method: 'GET',
+            url: 'https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&fm=jpg&fit=crop&w=1080&q=80&fit=max',
+            params: {
+            },
+            headers: {
+            }
+        };
+
+        axios.request(options)
+            .then((response) => {
+                let objRespuesta = {
+                    fotografia: response.data
+                }
+               res.write(JSON.stringify(objRespuesta));
+               res.end();
+            })
+            .catch((error) => {
+                console.log("Hubo un error en la aplicacion")
+                console.log(error);
+            });
+    }
+    else {
+        res.write(JSON.stringify({ data: "Faltan datos!" }));
+        res.end();
+    }
+
+}).listen(8080);
+
