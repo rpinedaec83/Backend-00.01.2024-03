5f11cd32b4d66f71f7c1656c0fb7edc85832c041
Roberto Pineda <rpineda@x-codec.net>
2024-05-16 22:05:32 -0500

    Clase 16052024 segunda parte

--- Semana09Sesion02/d19423/dml.sql
+++ Semana09Sesion02/d19423/dml.sql
@@ -53,3 +53,43 @@ from Orders O
 inner join Customers C on O.CustomerID = C.CustomerID
 inner join Employees E on O.EmployeeID = E.EmployeeID
 inner join Shippers S on O.ShipVia = S.ShipperID
+
+Select * from Customers
+Select * from Orders
+
+SELECT Customers.ContactName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.ContactName;
+
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+
+SELECT Customers.ContactName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.ContactName;
+
+Insert into Customers(CustomerID, CompanyName)
+values('XCODE', 'X-CODEC')
+Insert into Orders(EmployeeID)
+values( 2)
+
+
+SELECT * INTO CustomersBackup2017
+FROM Customers;
+
+select * from CustomersBackup2017
+
+
+select * from Customers
+where Country = 'USA'
+
+select top 5 count(quantity) cantidad,OD.ProductID, P.ProductName from [Order Details] OD
+inner join Products P on OD.ProductID = P.ProductID
+group by OD.ProductID,  P.ProductName
+order by count(quantity) desc
+
