df81cbc866af02935d927295a9f53a42a58fba2d
Marco <marco.poma1131@gmail.com>
2024-05-21 14:34:51 -0500

    2do Avance Hackaton 09

--- Hackaton09/SV72449754/Ejercicios.sql
+++ Hackaton09/SV72449754/Ejercicios.sql
@@ -200,50 +200,177 @@
 
 /*23. Seleccionar todos los campos de la tabla clientes,ordenar por compania  */
 
+		-- Consulta a la Pregunta
+		Select *from Customers order by CompanyName;
+
+
 /*24. Seleccionar todos los campos de clientes,cuya compania empiece con la letra B y 
 	  pertenezcan a UK ,ordenar por nombre de la compania  */
+
+	  -- Consulta a la Pregunta
+		Select *from Customers 
+		where (CompanyName like '[B]%') AND (Country='UK')
+		order by CompanyName;
+
+
 
 /*25. Seleccionar todos los campos de productos de las categorias 1,3 y 5
       ordenar por categoria  */
 
+	   -- Consultas iniciales
+	   Select *from Products;
+	   Select *from Categories;
+
+	   -- Consulta a la Pregunta
+		Select *From Products as p
+		inner join Categories as c on p.CategoryID=c.CategoryID
+		where (p.CategoryID=1) OR(p.CategoryID=3) OR (p.CategoryID=5);
+		
+
 /*26. Seleccionar los productos cuyos precios unitarios estan entre 50 y 200  */
+
+       -- Consulta a la Pregunta
+	   Select *from Products where UnitPrice between 50 and 200;
+	   
 
 /*27. Visualizar el nombre y el id de la compania del cliente,fecha,precio unitario y producto de la orden  */
 
+       -- Consultas iniciales
+	   Select *from Customers;
+	   Select *from Orders;
+	   Select *from [Order Details];
+
+	   -- Consulta a la Pregunta
+	   Select c.CustomerID as 'Codigo del Cliente',
+		   c.CompanyName as 'Compania del Cliente',
+		   o.OrderID as 'Codigo de Orden de Compra',
+		   o.OrderDate as 'Fecha de Orden de Compra',
+		   od.ProductID as 'Codigo de Producto',
+		   od.UnitPrice as 'Precio Unitario del Producto' 
+		   from Customers as c
+	   inner join Orders as o on c.CustomerID=o.CustomerID
+	   inner join [Order Details] as od on o.OrderID=od.OrderID
+
+
+
 /*28. Visualizar el nombre de la categoria y el numero de productos que hay por cada categoria.  */
+
+	   -- Consultas iniciales
+	   Select *from Categories;
+	   Select *from Products order by CategoryID;
+	  
+	   -- Consulta a la Pregunta
+	   Select c.CategoryID,c.CategoryName,Count(p.ProductID) as 'Cantidad de Productos' from Categories as c
+	   inner join Products as p on c.CategoryID=p.CategoryID
+	   Group by c.CategoryID,c.CategoryName;
+
 
 /*29. Seleccionar los 5 productos mas vendidos  */
 
+       -- Consultas iniciales
+	   Select *from [Order Details] order by ProductID;
+	   Select *from Products;
+
+	   -- Consulta a la Pregunta
+	   Select Top(5) od.ProductID,p.ProductName,sum(od.Quantity) as 'Total de productos vendidos' from [Order Details] as od
+	   inner join Products as p on od.ProductID=p.ProductID
+	   group by od.ProductID,p.ProductName 
+	   order by sum(od.Quantity) desc
+
+
+
 /*30. Seleccionar los jefes de los empleados  */
+
+       -- Consultas iniciales
+	   Select *from Employees;
+	  
 
 /*31. Obtener todos los productos cuyo nombre comienzan con M y tienen un precio comprendido entre 28 y 129  */
 
+       -- Consultas iniciales
+	   Select *from Products order by ProductName;
+
+	   -- Consulta a la Pregunta
+	   Select *from Products
+	   where (ProductName like '[M]%') AND (UnitPrice between 28 and 129)
+
+
 /*32. Obtener todos los clientes del Pais de USA,Francia y UK  */
+
+       -- Consultas iniciales
+	   Select *from Customers;
+
+	   -- Consulta a la Pregunta
+	   Select *from Customers
+	   where (Country='USA') or (Country='France') or (Country='UK')
+	   order by Country;
+
 
 /*33. Obtener todos los productos descontinuados o con stock cero.  */
 
+       -- Consultas iniciales
+	   Select *from Products order by ProductName;
+
+	   -- Consulta a la Pregunta
+	   Select *from Products
+	   where (UnitsInStock=0) or (Discontinued=1)
+	   order by ProductName
+
+
 /*34. Obtener todas las ordenes hechas por el empleado King Robert  */
+
+       -- Consultas iniciales
+	   Select *from Orders;
+	   Select *from Employees;
+
+	   -- Consulta a la Pregunta
+	   Select o.OrderID,o.EmployeeID,e.LastName+' ' +e.FirstName as 'Nombre del Empleado'
+	   From Orders as o
+	   inner join Employees as e on o.EmployeeID=e.EmployeeID
+	   where e.EmployeeID=7
+
 
 /*35. Obtener todas las ordenes por el cliente cuya compania es "Que delicia"  */
 
+       -- Consultas iniciales
+	   Select *from Orders order by CustomerID;
+	   Select *from Customers order by CompanyName;
+
+	   -- Consulta a la Pregunta
+	   Select o.OrderID,c.CustomerID,c.CompanyName from Orders as o
+	   inner join Customers as c on o.CustomerID=c.CustomerID
+	   where c.CustomerID='QUEDE'
+
+	   
 /*36. Obtener todas las ordenes hechas por el empleado King
 	  Robert,Davolio Nancy y Fuller Andrew  */
 
+	   -- Consultas iniciales
+	   Select *from Orders;
+	   Select *from Employees;
+
+	   -- Consulta a la Pregunta
+	   Select o.OrderID,o.EmployeeID,e.LastName+' ' +e.FirstName as 'Nombre del Empleado'
+	   From Orders as o
+	   inner join Employees as e on o.EmployeeID=e.EmployeeID
+	   where (e.EmployeeID=1) OR (e.EmployeeID=2) OR (e.EmployeeID=7)
+
+
 /*37. Obtener todos los productos(codigo,nombre,precio,stock) de la orden 10257*/
 
-    -- Consultas iniciales
-	select *from [Order Details] order by OrderID;  
-	select *from Products;							
+		-- Consultas iniciales
+		select *from [Order Details] order by OrderID;  
+		select *from Products;							
 
-	-- Consulta a la Pregunta
-	select od.OrderID,
-		od.ProductID,
-		p.ProductName,
-		od.Unitprice,
-		p.UnitsInStock from [Order Details] as od
-	inner join Products as p on od.ProductID=p.ProductID 
-		where od.OrderID=10257
-	group by od.OrderID,od.ProductID,p.ProductName,od.Unitprice,p.UnitsInStock;
+		-- Consulta a la Pregunta
+		select od.OrderID,
+			od.ProductID,
+			p.ProductName,
+			od.Unitprice,
+			p.UnitsInStock from [Order Details] as od
+		inner join Products as p on od.ProductID=p.ProductID 
+			where od.OrderID=10257
+		group by od.OrderID,od.ProductID,p.ProductName,od.Unitprice,p.UnitsInStock;
 			
 
 
@@ -426,7 +553,7 @@ código del producto, cantidad pedida del producto,nombre del producto, el nombr
   el código del producto,cantidad pedida del producto, nombre del producto y el nombre de la compañía proveedora, usas JOIN.Solamente 
   las compañías proveedoras que comienzan con la letra de la A hasta la letra G,además la cantidad pedida del producto debe estar entre 23 y 187.*/
  
-    -- Consultas iniciales
+        -- Consultas iniciales
 		select * from Customers;
 		select * from Orders order by CustomerID;
 		select * from [Order Details];
@@ -446,6 +573,6 @@ código del producto, cantidad pedida del producto,nombre del producto, el nombr
 			inner join [Order Details] as od on o.OrderID=od.OrderID
 			inner join Products as p on od.ProductID=p.ProductID
 			inner join Suppliers as s on p.SupplierID=s.SupplierID
-				Where c.CompanyName like '[A-G]%'
+				Where (c.CompanyName like '[A-G]%') AND (od.Quantity between 23 and 187 )
 		Group by c.CompanyName,c.ContactName,o.OrderID,o.OrderDate,od.ProductID,od.Quantity,
 				 p.ProductName,s.CompanyName order by c.CompanyName ;
