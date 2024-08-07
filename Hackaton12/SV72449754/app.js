a25b9330803733b8d246ccc7c57e9edc879c6749
MARCO <marco.poma1131@gmail.com>
2024-08-06 22:25:34 -0500

    3ro avance Hackaton-12

--- Hackaton12/SV72449754/app.js
+++ Hackaton12/SV72449754/app.js
@@ -6,6 +6,11 @@ let RegistroCompras=[];
 http.createServer(function (req, res) {
   const reqUrl = url.parse(req.url, true);
   console.log("reqUrl: ",reqUrl.pathname);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
  
   if (reqUrl.pathname === '/nuevo' && req.method === 'POST') {
 
@@ -13,9 +18,7 @@ http.createServer(function (req, res) {
 
     req.on('data', (chunk) => {
       console.log("capturando el chunk");
-      body += chunk;
-       
-    });
+      body += chunk;});
 
     req.on('end', () => {
       const lista = JSON.parse(body);
@@ -25,18 +28,25 @@ http.createServer(function (req, res) {
           fecha: lista.fecha,
           estado: lista.estado
       });
-      console.log("lista: ",RegistroCompras);
+      console.log("Registros Creados: ",RegistroCompras);
     });
-
-       
+  
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Registro creado' }));
   }
 
-  else if (reqUrl.pathname === '/pendiente' && req.method === 'GET') {
-    const completados = RegistroCompras.filter(elem => elem.estado);
+  else if (reqUrl.pathname === '/pendientes' && req.method === 'GET') {
+    const Pendientes = RegistroCompras.filter(elem => !elem.estado);
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(completados));    
+    res.end(JSON.stringify(Pendientes));  
+    console.log("listas Pendientes: ",Pendientes);  
+  }
+
+  else if (reqUrl.pathname === '/completados' && req.method === 'GET') {
+    const Completados = RegistroCompras.filter(elem => elem.estado);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(Completados));  
+    console.log("listas Completados: ",Completados);  
   }
 
 
--- Hackaton12/SV72449754/script.js
+++ Hackaton12/SV72449754/script.js
@@ -1,60 +1,88 @@
+let $tableAreaPendientes = $('#tableAreaPendientes');
+let $tableAreaCompletados = $('#tableAreaCompletados');
+let RegistroPendientes=[];
+let RegistroCompletados=[];
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Se cargo el DOM");
     const RegistroNuevo = document.getElementById("RegistroForm");
     const Registropendientes = document.getElementById("btnPendientes");
-    const Registrocompletados = document.getElementById("btnCompletos");
+    const Registrocompletados = document.getElementById("btnCompletados");
+    $tableAreaPendientes.bootstrapTable({ data:RegistroPendientes});
+    $tableAreaCompletados.bootstrapTable({ data:RegistroCompletados});
 
     RegistroNuevo.addEventListener("submit",async (e)=>{
         e.preventDefault();
-        console.log("Envio datos desde el formulario");
         const Obj=new FormData(RegistroNuevo);
-
         const RegCompraNueva={
             nombre:      Obj.get("nombre"),
             descripcion: Obj.get("descripcion"),
             fecha:       Obj.get("fecha"),
             estado:      Obj.get("estado") ? true: false
         };
-        console.log(RegCompraNueva);
-
+        
         try {
-          console.log("enviando datos al servidor-0");
           const response = await fetch("http://localhost:8080/nuevo" ,{
               method: "POST",
-              mode:"no-cors",
               headers: {"Content-Type": "application/json",},
               body: JSON.stringify(RegCompraNueva),   
           });
 
-          console.log("enviando datos al servidor-1",response.ok);
-          try {
-            console.log("envio exitoso");
-            const data =  await response.json();
-            console.log("Recibido del Server: ",data);
-          } catch (error) {
-            console.log("error: ",error)
-          }
-          console.log("enviando datos al servidor-2");
-            
+          console.log("\nPOST--->envio exitoso");
+          console.log(RegCompraNueva);
+          const data =  await response.json();
+          console.log("<---Recibido del POST-Server: ",data);
+          console.log("");
           } catch (error) {}
-
-       /* fetch("http://localhost:8080/nuevo" ,{
-            method: 'POST',
-            mode:"no-cors",
-            headers: {'Content-type': 'application/json; charset=UTF-8',},
-            body: JSON.stringify(RegCompraNueva),})
-        .then((response) => response.json())
-        .then((json) => console.log(json.value));*/
-       
     });
 
     Registropendientes.addEventListener("click",async (e)=>{
       e.preventDefault();
-      await fetch("http://localhost:8080/pendiente");  
-    }
-    );
+      
+      try {
+        const response= await fetch("http://localhost:8080/pendientes",{
+          method: 'GET',
+          headers: {'Content-type': 'application/json; charset=UTF-8',},
+        });
+        console.log("\n<---Recibido del GET Pendientes-Server: ");
+        const data =  await response.json();
+        console.log("Data: ",data);
+        if (data.length>0) {
+          data.forEach(elem => {
+            console.log(elem);
+          });
+          RegistroPendientes=data;
+          console.log("RegistroPendientes: ",RegistroPendientes);
+        }
+        else{console.log("{}");}
+        console.log("");
+        $tableAreaPendientes.bootstrapTable('load', RegistroPendientes);
+      } catch (error) {}
+    });
 
 
+    Registrocompletados.addEventListener("click",async (e)=>{
+      e.preventDefault();
 
+      try {
+        const response= await fetch("http://localhost:8080/completados",{
+          method: 'GET',
+          headers: {'Content-type': 'application/json; charset=UTF-8',},
+        });
+        console.log("\n<---Recibido del GET Completados-Server: ");
+        const data =  await response.json();
+        if (data.length>0) {
+          data.forEach(elem => {
+            console.log(elem);
+          });
+          RegistroCompletados=data;
+          console.log("RegistroCompletados: ",RegistroCompletados);
+        }
+        else{console.log("{}");}
+        console.log("");
+        $tableAreaCompletados.bootstrapTable('load', RegistroCompletados);
+      } catch (error) {}
+    });
 
   });
+
+
